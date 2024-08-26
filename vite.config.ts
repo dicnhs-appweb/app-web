ae89c608614622249eb506d52dc5147c9c295f00
Aidre Cabrera <aidrecabrera@gmail.com>
2024-08-27 00:35:11 +0800

    vite config restoration

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import {TanStackRouterVite} from '@tanstack/router-plugin/vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import Unfonts from 'unplugin-fonts/vite';
+import {defineConfig} from 'vite';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    TanStackRouterVite(),
+    Unfonts({
+      custom: {
+        families: [
+          {
+            name: 'Geist',
+            src: './src/assets/geist/*.woff2',
+          },
+        ],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
