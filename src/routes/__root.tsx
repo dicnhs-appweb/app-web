7a9be16b439e4a181f9fad142e93f1c21faffeb0
Aidre Cabrera <aidrecabrera@gmail.com>
2024-08-27 00:47:11 +0800

    mode toggle

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,3 +1,4 @@
+import {ModeToggle} from '@/components/mode-toggle'
 import {QueryClient} from '@tanstack/react-query'
 import {Outlet, createRootRouteWithContext} from '@tanstack/react-router'
 
@@ -9,6 +10,9 @@ export const Route = createRootRouteWithContext<UserRouteContext>()({
   component: () => (
     <div className="mx-6">
       <Outlet />
+      <div className="fixed z-10 bottom-4 left-4 sm:bottom-6 sm:left-6">
+        <ModeToggle />
+      </div>
     </div>
   ),
 })
